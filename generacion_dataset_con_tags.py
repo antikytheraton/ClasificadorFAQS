0b039615abd2aff41c5c00fc868fb4d8e7765de0
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2017-06-20 15:46:33 -0500

    Se documento la primera parte del algoritmo

--- generacion_dataset_con_tags.py
+++ generacion_dataset_con_tags.py
@@ -1,4 +1,4 @@
-#from aumenta_datos import aumentar_data_set
+"""Clase que te ayuda a 'inflar' tu dataset"""
 from aumenta_datos import aumentar_data_set
 import unicodedata
 
@@ -6,6 +6,11 @@ import unicodedata
 def elimina_tildes(s):
    return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
 
+#Declaración de los parámetros para inflar nuestro dataset
+#Frase: Es la frase base donde los campos que queremos sustituir están en mayúsculas
+#Hueco: Es el campo que queremos sustituir, recuerda que debe de ir en mayúscula
+#Valores: Son los valores con los que quieres sustituir los huecos
+#IMPORTANTE: el orden de arreglo de valores, y de huecos deben de coincidir.
 
 topicos = ['dinero', 'dinero sucio', 'recibo', 'deposito', 'beneficiario', 'cuenta', 'cuenta compartida']
 acciones = ['enviar', 'recibir', 'obtener exitosamente', 'cancelar', 'pagar']
@@ -17,6 +22,8 @@ huecos = ['TOPICO', 'ACCION', 'UN']
 frases = []
 numeros_intencion = []
 
+#En este caso las frases a inflar las sacamos de un archivo txt
+#Las frases infladas se guardan en frases y sus número de intencion en numeros_intencion
 file = open("trainingFAQs.txt", 'r')
 for linea in file.readlines():
     frase = linea
@@ -28,17 +35,14 @@ for linea in file.readlines():
     numeros_intencion.append(numero_intencion*len(aumentar_data_set(frase_sin_numero, huecos, valores)))
 file.close()
 
-
+#Se ingresa el numero de intencion con la frase inflada correspondiente, y se escribe a otro tx
 numeros_intencion = ''.join(numeros_intencion)
 file = open("data_final_chido.txt", 'w')
-i = 0
+index_numeros_intencion=0
 for oraciones in frases:
     for palabra in oraciones:
         oracion = " ".join(str(x) for x in palabra)
-        file.write(oracion+numeros_intencion[i])
+        file.write(oracion+numeros_intencion[index_numeros_intencion])
         file.write('\n')
-        i+=1
+        index_numeros_intencion+=1
 file.close()
-
-#frases = aumentar_data_set(frase, huecos, valores)
-#print(frases)
